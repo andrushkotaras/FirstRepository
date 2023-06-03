f7cf6323f3b7f20c5b018a5428888025885e29e8
andrushkotaras <tarasandrushko2000@gmail.com>
2023-06-03 14:17:04 +0300

    Update README.md

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int a = 3;
+        int b = 4;
+        int suma = a + b;
+        int riznytsia = a - b;
+        int dobutot = a * b;
+        double chastka = a % b;
+        System.out.println("Сума чисел " + suma);
+        System.out.println("Різниця чисел " + riznytsia);
+        System.out.println("Добуток чисел " + dobutot);
+        System.out.println("Залишок після ділення чисел " + chastka);
     }
 }
