cc757d284bb1b8e45a3c113d420f908ac1a96bd8
andrushkotaras <tarasandrushko2000@gmail.com>
2023-06-03 20:10:17 +0300

    Update README.md

--- Main.java
+++ Main.java
@@ -10,5 +10,11 @@ public class Main {
         System.out.println("Різниця чисел " + riznytsia);
         System.out.println("Добуток чисел " + dobutot);
         System.out.println("Залишок після ділення чисел " + chastka);
+
+        String word ="Word";
+        System.out.println(word.toLowerCase());
+        System.out.println(word.toUpperCase());
+        System.out.println(word.charAt(0));
+        System.out.println(word.charAt(3));
     }
 }
