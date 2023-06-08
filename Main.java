7c64e71f16284962d5a9544bbddb9e66a21442d4
andrushkotaras <tarasandrushko2000@gmail.com>
2023-06-08 16:16:02 +0300

    Update README.md

--- Main.java
+++ Main.java
@@ -1,20 +1,44 @@
-public class Main {
-    public static void main(String[] args) {
-        int a = 3;
-        int b = 4;
-        int suma = a + b;
-        int riznytsia = a - b;
-        int dobutot = a * b;
-        double chastka = a % b;
-        System.out.println("Сума чисел " + suma);
-        System.out.println("Різниця чисел " + riznytsia);
-        System.out.println("Добуток чисел " + dobutot);
-        System.out.println("Залишок після ділення чисел " + chastka);
+import java.util.Scanner;
+  public class Main {
+      public static void main(String[] args) {
+          bilshe(3, 4);
+          menshe(6,7);
+          int p1;
+          System.out.println("Введіть число");
+          Scanner input = new Scanner(System.in);
+          p1 = input.nextInt();
 
-        String word ="Word";
-        System.out.println(word.toLowerCase());
-        System.out.println(word.toUpperCase());
-        System.out.println(word.charAt(0));
-        System.out.println(word.charAt(3));
-    }
-}
+      }
+
+      private static void bilshe(int b1, int b2) {
+          if (b1 > b2) {
+              System.out.println("b1 > b2");
+          }
+          if (b1 < b2) {
+              System.out.println("b1 < b2");
+          } else {
+              System.out.println("b1 = b2");
+          }
+
+      }
+
+      private static void menshe (int m1, int m2 ){
+          if (m1 < m2) {
+              System.out.println("m1 < m2");
+          }
+          if (m1 > m2) {
+              System.out.println("m1 > m2");
+          } else {
+              System.out.println("m1 = m2");
+          }
+      }
+
+      private static void parnist(int p1) {
+          if (p1 % 2 == 0) {
+              System.out.println("Число парне");
+          } else {
+              System.out.println("Число не парне");
+          }
+
+      }
+  }
