07ba5ea0cf592ed862b33c1fffea796fe3a049fc
andrushkotaras <tarasandrushko2000@gmail.com>
2023-06-22 19:22:54 +0300

    Update README.md

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
- public class Main {
+import java.util.Scanner;
+public class Main {
       public static void main(String[] args) {
-
+          //Дз №6
           // Завдання 1
           for (int number = 1; number <= 100; number = number + 1){
               System.out.println(number);
@@ -30,5 +31,48 @@
               if (f2 > 100)
                   break;
               System.out.println(f2); }
+
+          //Дз №7
+          // Завдання 1
+          System.out.print("Введіть число: ");
+          Scanner sc = new Scanner(System.in);
+          int scanNumber = sc.nextInt();
+          System.out.println(cub(scanNumber));
+
+          // Завдання 2
+          System.out.print("Кількість зірочок: ");
+          Scanner st = new Scanner(System.in);
+          int stars = st.nextInt();
+          starsMethod(stars);
+
+          // Завдання 3
+          System.out.println("Введіть  число повторювань символу: ");
+          Scanner tim = new Scanner(System.in);
+          int times = tim.nextInt();
+
+          symbols(times);
+      }
+
+
+      public static int cub (int scanNumber){
+          return scanNumber * scanNumber * scanNumber;
+      }
+      public static void starsMethod (int stars){
+          String star = "*";
+          if (stars > 0) {
+              System.out.println(star);
+              stars--;
+              starsMethod(stars);
+          }
+      }
+      public static void symbols (int times){
+          System.out.print("Введіть символ: ");
+          Scanner symb = new Scanner(System.in);
+          String symbol = symb.next();
+
+          while (times > 0){
+              System.out.println(symbol);
+              times--;
+          }
       }
   }
