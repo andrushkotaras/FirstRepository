7bf083df1088dd5be733d90beeafe1ec5d66692a
andrushkotaras <tarasandrushko2000@gmail.com>
2023-06-23 19:03:39 +0300

    Update README.md

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Random;
 public class Main {
       public static void main(String[] args) {
+          /*
           //Дз №6
           // Завдання 1
           for (int number = 1; number <= 100; number = number + 1){
@@ -51,7 +55,41 @@ public class Main {
           int times = tim.nextInt();
 
           symbols(times);
+
+           */
+
+          //Дз №8
+          // Завдання 1
+          Random random = new Random();
+          final int range = 100;
+          int [] masyv = new int[10];
+          for (int g = 0; g<10; g++){
+              masyv [g] = random.nextInt(range);
+          }
+
+          // Завдання 2
+          System.out.println(Arrays.toString(masyv));
+
+          // Завдання 3
+          Arrays.sort(masyv);
+          System.out.println(masyv[0]);
+
+          // Завдання 4
+          System.out.println(masyv[9]);
+
+          // Завдання 5
+          double suma = 0;
+          for (int m = 0; m<10; m++){
+              suma = suma + masyv[m];
+          }
+          System.out.println("Сума елементів масиву - " + suma);
+
+          // Завдання 6
+          System.out.println("Середнє значення елементів масиву - " + suma / masyv.length);
+
       }
+
+    /*
 
 
       public static int cub (int scanNumber){
@@ -75,4 +113,6 @@ public class Main {
               times--;
           }
       }
+
+     */
   }
