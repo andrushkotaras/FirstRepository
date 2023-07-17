76be0a2f2b51f91e279e5b08e01c2d0e3d50a609
andrushkotaras <tarasandrushko2000@gmail.com>
2023-07-17 17:08:46 +0300

    Update README.md

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 public class Main {
         public static void main(String[] args) {
-                Human pilot = new Pilot();
-                ((Pilot) pilot).theWholeFlight();
+                // При створенні обєкту від більш абстрактного класу (Human)
                 Human driver = new Driver();
+                // Мені пропонує такий варіант викликання методу:
                 ((Driver) driver).theWholeTrip();
+
+                Pilot pilot = new Pilot();
+                pilot.theWholeFlight();
+
         }
 }
 
